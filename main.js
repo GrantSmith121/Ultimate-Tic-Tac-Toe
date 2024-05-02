38b9224157ea33d77ef2093ec10ddd96ea88bb3f
GrrantSmith121 <gss1@iu.edu>
2024-05-02 14:33:37 -0400

    Ensuring empty spaces don't count towards victory
    Each checker function now ensures that an empty row does not evaluate to true

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let spaces = [[null, null, null], [null, null, null], [null, null, null]];
+let testSpaces = [["X", "X", "X"], [null, null, null], [null, null, null]];
+let testSpaces1 = [["X", null, null], ["X", null, null], ["O", null, null]];
+let testSpaces2 = [["X", null, null], [null, "X", null], [null, null, "X"]];
 
 // 0=continue 1=win 2=lose
 let winCondition = 0;
@@ -26,7 +29,16 @@ function place(location, mark) {
     column = mark;
 }
 
+
+
 function rowCheck(row) {
+    if (row[0] === null || row[1] === null || row[2] === null) {
+        // return false;
+        // console.log("nope");
+        return false;
+    }
+    // else return rowChecker(row);
+    // console.log(rowChecker(row));
     if (row[0] != row[1]) {
         return false;
     }
@@ -38,6 +50,10 @@ function rowCheck(row) {
 
 // grid is the set of all given spaces and index is which column you want to check
 function columnCheck(grid, index) {
+    if ((grid[0])[index] === null || (grid[0])[index] === null || (grid[0])[index] === null) {
+        return false;
+    }
+
     if ((grid[0])[index] != (grid[1])[index]) {
         return false;
     }
@@ -49,8 +65,14 @@ function columnCheck(grid, index) {
 
 // leftToRight is a boolean to check if you are going diagonal from left to right or right to left
 function diagonalCheck(grid, leftToRight) {
+    // if ((grid[0])[0] === null || (grid[1])[1] === null || (grid[2])[2] === null || (grid[0])[2] === null || (grid[2])[0] === null) {
+    //     return false;
+    // }
     // condition for if we are checking from left to right
     if (leftToRight == true) {
+        if ((grid[0])[0] === null || (grid[1])[1] === null || (grid[2])[2] === null) {
+            return false;
+        }
         if ((grid[0])[0] != (grid[1])[1]) {
             return false;
         }
@@ -61,6 +83,9 @@ function diagonalCheck(grid, leftToRight) {
     }
     // condition for if we are checking from right to left
     else {
+        if ((grid[0])[2] === null || (grid[1])[1] === null || (grid[2])[0] === null) {
+            return false;
+        }
         if ((grid[0])[2] != (grid[1])[1]) {
             return false;
         }
