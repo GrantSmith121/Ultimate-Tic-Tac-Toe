618b3705d94910adb401266d3331dfb512b8a10a
GrrantSmith121 <gss1@iu.edu>
2024-05-04 10:56:19 -0400

    Place function is now functional

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let spaces = [[null, null, null], [null, null, null], [null, null, null]];
 let testSpaces = [["X", "X", "X"], [null, null, null], [null, null, null]];
 let testSpaces1 = [["X", null, null], ["X", null, null], ["O", null, null]];
 let testSpaces2 = [["X", null, null], [null, "X", null], [null, null, "X"]];
+let testSpaces3 = [[null, null, "X"], [null, "X", null], ["X", null, null]];
 
 // 0=continue 1=win 2=lose
 let winCondition = 0;
@@ -13,20 +14,15 @@ let yourTurn;
 //location[2, 1]
 
 // function to place either X or O in a specific space
-function place(location, mark) {
+function place(grid, location, mark) {
 
-    //let rowNum = grid[rowPOS]; // val = grid[2] = row2 = [0, 0, 0]
-    //let column = rowNum[columnPOS]; // val = 0
+    let rowNum = location[0];
+    let columnNum = location[1];
 
-    let row = location[0];
-    let column = location[1];
+    let row = grid[rowNum];
 
-    let rowInColumn = row[column];
-
-    //spaces[(location[0])[(location[1])]];
-    console.log(spaces[rowInColumn]);
-
-    column = mark;
+    row[columnNum] = mark;
+    console.log(row[columnNum]);
 }
 
 
