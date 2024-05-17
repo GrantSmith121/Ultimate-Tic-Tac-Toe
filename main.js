f6556e98a53c49222f1305d8a0f7542d4cc4165b
Grant Smith <70243470+GrantSmith121@users.noreply.github.com>
2024-05-17 14:09:44 -0400

    Completed basic tic tac toe gamestate
    * Created object to house available spaces and player turn
    
    * Creating basic UI for game
    
    * Spaces now hover when moused over
    
    * Space 0-0 now places O when you click it
    
    * Uploading small changes for laptop work
    
    Making sure my laptop can receive the most up-to-date code
    
    * O's now get placed in any spot the user clicks
    
    * Space can now only be played if no other value exists
    
    * Winner function now works
    
    * Win check now triggers whenever player places "O"
    
    * Ensuring win function can let either player win
    
    * ai player now plays when user plays
    
    * UI for player X now correct
    
    * AI player waits a bit before playing
    
    * X can no longer play once O has won
    
    * Game state now correctly reports when O wins
    
    * Getting rid of unneeded console logs
    
    * User now can't play after the game has ended
    
    * Last place that the player plays also toggles after win
    
    * aiPlay no longer calls infinitely if no playable spaces are left
    
    * O player now can't play while AI player is still playing
    
    * Game now has a start button.
    
    The player cannot make any moves before the start button is pressed
    
    * Play button now properly resets the game
    
    * Play again button now displays current winner
    
    * anySpacesEmpty no longer incorrectly returns false

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-let spaces = [[null, null, null], [null, null, null], [null, null, null]];
+const gridElement = document.getElementById("grid");
+const playButton = document.getElementById("play-button");
+
+const board = {
+    spaces: [[null, null, null], [null, null, null], [null, null, null]],
+    turn: 1,
+    winner: "",
+    gameActive: false
+}
+
+let spacesBaseCase = [[null, null, null], [null, null, null], [null, null, null]];
 let testSpaces = [["X", "X", "X"], [null, null, null], [null, null, null]];
 let testSpaces1 = [["X", null, null], ["X", null, null], ["O", null, null]];
 let testSpaces2 = [["X", null, null], [null, "X", null], [null, null, "X"]];
@@ -7,7 +17,36 @@ let testSpaces3 = [[null, null, "X"], [null, "X", null], ["X", null, null]];
 // 0=continue 1=win 2=lose
 let winCondition = 0;
 
-let yourTurn;
+function toggleAllSpaces() {
+    let indexValues = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
+    for (let i = 0; i < indexValues.length; i++ ){
+        let space = document.getElementById(indexValues[i]);
+        if (space.innerText === "") {
+            space.classList.toggle('hover-effect');
+        }
+    }
+}
+
+toggleAllSpaces();
+
+function emptyAllSpaces() {
+    let indexValues = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
+    for (let i = 0; i < indexValues.length; i++ ){
+        let space = document.getElementById(indexValues[i]);
+        space.innerText = "";
+    }
+}
+
+playButton.addEventListener("click", function() {
+    // console.log(playButton.style.display);
+    playButton.style.display = "none";
+    board.gameActive = true;
+    board.spaces = [[null, null, null], [null, null, null], [null, null, null]];
+    board.turn = 1;
+    board.winner = "";
+    emptyAllSpaces();
+    toggleAllSpaces();
+})
 
 // location is an array
 // the first is row coordinate and the second value is column coordinate
@@ -16,16 +55,62 @@ let yourTurn;
 // function to place either X or O in a specific space
 function place(grid, location, mark) {
 
+    if (grid.winner != "") {
+        console.log(mark + " wins!");
+        return;
+    }
+
+    if (grid.gameActive === false) {
+        return;
+    }
+
     let rowNum = location[0];
     let columnNum = location[1];
 
-    let row = grid[rowNum];
+    let row = grid.spaces[rowNum];
 
     row[columnNum] = mark;
     console.log(row[columnNum]);
+    console.log("space" + rowNum + columnNum);
+
+    winCheck(grid, mark);
+
+
+
+    if (grid.turn === 1) {
+        grid.turn = 2;
+        aiPlay();
+    } else { grid.turn = 1 }
+
+    //let indexValues = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
+    // return true;
+    if (grid.winner != "") {
+        console.log(grid.winner + " wins!");
+        let space = document.getElementById("" + location[0] + location[1]);
+        space.classList.toggle('hover-effect');
+        
+        toggleAllSpaces();
+        // for (let i = 0; i < indexValues.length; i++ ){
+        //     let space = document.getElementById(indexValues[i]);
+        //     if (space.innerText === "") {
+        //         space.classList.toggle('hover-effect');
+        //     }
+        // }
+    }
+    return true;
 }
 
-
+gridElement.addEventListener('click', function(event) {
+    if (event.target.classList.contains('space') && board.turn === 1 && board.gameActive === true) {
+         const space = document.getElementById(event.target.getAttribute('id'));
+        if (space.innerText === "" && board.winner === "") {
+            const location = [Number((event.target.getAttribute('id')).substr(0, 1)), Number((event.target.getAttribute('id')).substr(1, 2))];
+            place(board, location, "O");
+            space.innerText = "O";
+            space.classList.toggle('hover-effect');
+        }
+    }
+});
 
 function rowCheck(row) {
     if (row[0] === null || row[1] === null || row[2] === null) {
@@ -79,8 +164,82 @@ function diagonalCheck(grid) {
 
 }
 
-// function winCheck(spaces) {
-//     for(let i = 0; i <= 2; i++) {
-//         if (spaces[i])
-//     };
-// }
+
+
+
+// player is either "X" or "O"
+function winCheck(grid, player) {
+    for(let i = 0; i <= 2; i++) {
+        rowCheck(grid.spaces[i]);
+        if (rowCheck(grid.spaces[i])) {
+            grid.winner = player;
+            playButton.style.display = "block";
+            playButton.textContent = "" + board.winner + " wins! Play again?";
+            // console.log("Player 1 wins!");
+            return;
+        }
+        columnCheck(grid.spaces, i);
+        if (columnCheck(grid.spaces, i)) {
+            grid.winner = player;
+            playButton.style.display = "block";
+            playButton.textContent = "" + board.winner + " wins! Play again?";
+            // console.log("Player 1 wins!");
+            return;
+        }
+    };
+    diagonalCheck(grid.spaces);
+    if (diagonalCheck(grid.spaces)) {
+        grid.winner = player;
+        playButton.style.display = "block";
+        playButton.textContent = "" + board.winner + " wins! Play again?";
+        // console.log("Player 1 wins!");
+        return;
+    };
+}
+
+function anySpacesEmpty() {
+
+    for (let i = 0; i <= 2; i++) {
+        if (board.spaces[i][0] === null) {
+            return true;
+        }
+        if (board.spaces[i][1] === null) {
+            return true;
+        }
+        if (board.spaces[i][2] === null) {
+            return true;
+        }
+    } return false;
+}
+
+function aiPlay() {
+    if (board.winner != "") {
+        return;
+    }
+
+    let location = [Math.floor(Math.random() * 3), Math.floor(Math.random() * 3)];
+    // the function found an empty space
+
+    if (anySpacesEmpty()) {
+        if (board.spaces[location[0]][location[1]] === null) {
+            setTimeout(() => {  
+                place(board, location, "X");
+                const space = document.getElementById("" + location[0].toString() + location[1].toString());
+                space.innerText = "X";
+                space.classList.toggle('hover-effect'); 
+            }, 1000);
+    
+            return;
+        // the function found a space already filled
+         } else { 
+            location = [Math.floor(Math.random() * 3), Math.floor(Math.random() * 3)]; 
+            aiPlay();
+        }
+    } else {
+        board.winner = "No one";
+        playButton.style.display = "block";
+        playButton.textContent = "" + board.winner + " wins! Play again?";
+        return;
+    }
+    
+}
